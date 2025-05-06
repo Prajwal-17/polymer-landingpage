371037331149cf7fba65a8c3e253e899490572dc
prajwal <prajwalkreddy17@gmail.com>
2025-05-07 00:41:24 +0530

    added new image

--- app/page.tsx
+++ app/page.tsx
@@ -6,38 +6,52 @@ import { ProductInfo } from "@/components/ProductInfo";
 import { SettingComponent } from "@/components/SettingComponent";
 import { SignUpCard } from "@/components/SignUpCard";
 import { FooterCard } from "@/components/FooterCard";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="h-auto w-full px-3 py-4">
-      <section className="h-[110vh] w-full rounded-xl bg-pink-200 px-28">
-        <div className="mx-16 flex items-center justify-center pt-8">
-          <Navbar />
-        </div>
-        <div className="space-y-5 py-28 text-center">
-          <div className="font-medium text-[#555555]">Introducing Polymer</div>
-          <div className="text-5xl font-bold">Hiring made simple</div>
-          <div className="mx-auto max-w-2xl text-lg font-medium">
-            <span>
-              Polymer gives you a beautiful site to display your job openings
-              and powerful applicant tracking software for candidate
-              relationship management.
-            </span>
+      <div className="relative">
+        <section className="h-[150vh] w-full rounded-xl bg-pink-200 px-28">
+          <div className="mx-16 flex items-center justify-center pt-8">
+            <Navbar />
           </div>
-          <div className="mt-5 flex items-center justify-center gap-3 font-medium">
-            <button className="rounded-lg bg-black px-5 py-2 text-white hover:cursor-pointer hover:bg-gray-800">
-              Sign up for free
-            </button>
-            <button className="rounded-lg bg-black/5 px-5 py-2 font-semibold text-black hover:cursor-pointer hover:bg-black/10">
-              Contact us
-            </button>
+          <div className="space-y-5 py-28 text-center">
+            <div className="font-medium text-[#555555]">
+              Introducing Polymer
+            </div>
+            <div className="text-5xl font-bold">Hiring made simple</div>
+            <div className="mx-auto max-w-2xl text-lg font-medium">
+              <span>
+                Polymer gives you a beautiful site to display your job openings
+                and powerful applicant tracking software for candidate
+                relationship management.
+              </span>
+            </div>
+            <div className="mt-5 flex items-center justify-center gap-3 font-medium">
+              <button className="rounded-lg bg-black px-5 py-2 text-white hover:cursor-pointer hover:bg-gray-800">
+                Sign up for free
+              </button>
+              <button className="rounded-lg bg-black/5 px-5 py-2 font-semibold text-black hover:cursor-pointer hover:bg-black/10">
+                Contact us
+              </button>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section className="mt-5 h-[590px] rounded-xl bg-[#F4F4F4]">
-        <CompaniesComponent />
-      </section>
+        <div className="absolute left-1/2 top-1/2 z-10 h-[1000px] w-[1100px] -translate-x-1/2 -translate-y-[45%]">
+          <Image
+            src="/billboard.webp"
+            alt="Billboard Image"
+            fill
+            className="object-contain"
+          />
+        </div>
+
+        <section className="mt-5 h-[590px] rounded-xl bg-[#F4F4F4]">
+          <CompaniesComponent />
+        </section>
+      </div>
 
       <section className="mt-5 h-[590px] w-full rounded-xl bg-pink-200">
         <ProductInfo />
